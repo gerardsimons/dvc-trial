753328147a29fa74b3f911e7dece6a78889d4418
Gerard Simons <sjrasimons@gmail.com>
2023-07-25 09:57:12 +0200

    Refactor data folder for csv

--- prepare.py
+++ prepare.py
@@ -1,6 +1,8 @@
+#!/
+
 import pandas as pd
 
 if __name__ == '__main__':
-	df = pd.read_csv('raw.csv')
+	df = pd.read_csv('data/raw.csv')
 	df = df * 10
-	df.to_csv('prepared.csv', index=False)
+	df.to_csv('data/prepared.csv', index=False)
